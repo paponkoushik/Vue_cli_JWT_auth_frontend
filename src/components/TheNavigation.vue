a389793a3b39e35c085afdd4142bc1b10e7a46d1
paponkoushik <paponkoushik@gmail.com>
2021-05-08 07:43:53 +0600

    deleted unnecessary code and added navigation bar component

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+<!--  <div>nav</div>-->
+  <ul>
+    <li>
+      <router-link :to="{name: 'Home'}">Home</router-link>
+    </li>
+    <li>
+      <router-link :to="{name: 'Login'}">Login</router-link>
+    </li>
+    <li>
+      Koushik Balo
+    </li>
+    <li>
+      <router-link :to="{name: 'Login'}">Login</router-link>
+    </li>
+
+  </ul>
+</template>
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "Nav"-->
+<!--}-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">Dashboard</div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
